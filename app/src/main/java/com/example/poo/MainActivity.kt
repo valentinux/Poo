95c26dc22af6425f35ade66581e5e7aa76576ce0
Valentinux <valentinux81@gmail.com>
2022-07-31 10:51:35 +0200

    workin with the class Phone

--- app/src/main/java/com/example/poo/MainActivity.kt
+++ app/src/main/java/com/example/poo/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.poo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.poo.classes.Phone
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        main()
+    }
+
+    val phone = Phone(942884132)
+
+    fun main(){
+        phone.call()
+        phone.shownumber()
+        println("The calling number is: " + phone.number)
     }
 }
---
+++ app/src/main/java/com/example/poo/classes/Phone.kt
@@ -0,0 +1,12 @@
+package com.example.poo.classes
+
+class Phone (val number: Int) {
+
+    fun call(){
+        println("Calling....")
+    }
+
+    fun shownumber(){
+        println(number)
+    }
+}
